0f79bf68c6de98f24460163f0719ffdef2623b7e
Nazarets <ksenianazarec995@mail.com>
2024-06-10 18:02:05 +0300

    string prjct updated

--- Fraction/main.cpp
+++ Fraction/main.cpp
@@ -2,10 +2,11 @@
 #include<iostream>
 using namespace std;
 //:: -  scope operator(оператор разрешения видимости) - позволяет "зайти" в пространство имён
-//namespace( пространство имён) как папка, а имя, расположенное в нём - как файл.
+//namespace(пространство имён) как папка, а имя, расположенное в нём - как файл.
 // сам по себе :: выводит нас в Global scope(глобальное пространство имён)
-// cout - console out ; cin 
+// cout - console out ; cin - console in 
 #define delimeter "\n-----------------------------------\n"
+#define dbldelimeter "\n==================================\n"
 class Fraction;
 Fraction operator*(Fraction A, Fraction B);
 Fraction operator+(const Fraction& A, const Fraction& B);
@@ -16,189 +17,191 @@ class Fraction
 	int num;
 	int den;
 public:
-	      // INCAPSULATION
-int getNUM()const
-{
-	return num;
-}
-int getDEN()const
-{
-	return den;
-}
-int getINT()const
-{
-	return intpart;
-}
-void setNUM(int num)
-{
-	this->num = num;
-}
-void setDEN(int den)
-{
-	if (den == 0)this->den = 1;
-	else this->den = den;
-}
-void setINT(int intpart)
-{
-	this->intpart = intpart;
-}
-          // CONSTRUCTOR
-Fraction()
-{
-	intpart = 0;
-	num = 0;
-	den = 1;
-	cout << "DefaultConstructor: \t" << this << endl;
-}
-Fraction(int numenator, int denominator)
-{
-	intpart = 0;
-	num = numenator;
-	this->setDEN(denominator);
-	cout << "Constructor(NoInteger): \t" << this << endl;
-}
-Fraction(int intpart)
-{
-	this->intpart = intpart;
-	num = 0;
-	den = 1;;
-	cout << "Constructor(NoFraction): \t" << this << endl;
-}
-Fraction(int intpart, int numenator, int denominator)
-{
-	this->intpart = intpart;
-	this->num = numenator;
-	setDEN(denominator);
-	cout << "Constructor: \t" << this << endl;
-}
-Fraction(const Fraction& other)
-{
-	this->intpart = other.intpart;
-	this->num = other.num;
-	this->den = other.den;
-	cout << "CopyConstructor: \t" << this << endl;
-}
-         //DESTRUCTOR
-~Fraction()
-{
-	cout << "Destructor: " << this << endl;
-}
-         // METHODS
-void simplify()
-{
-	for (int i = (abs(num) < abs(den) ? abs(num) : abs(den));i>0;i--)
-	if(num % i == 0 && den % i == 0)
-		{
-			num /= i;
-			den /= i;
-			break;
-		}
-}
-Fraction& reduce()
-{
-	int more, less, rest;
-	if (num < den)more = num,less = den;
-	else more = den,less = num;
-
-	do 
+	// INCAPSULATION
+	int getNUM()const
 	{
-		rest = more % less;
-		more = less;
-		less = rest;
-	} while (rest);
-	int GSD = more; // gsd - greatest common divisor
-	num /= GSD;
-	den /= GSD;
-	return* this;
-}
-Fraction& to_improper()
-{
-	num = intpart * den+(intpart >= 0? num:-num);
-	intpart = 0;
-	return *this;
-}
-Fraction& to_proper()
-{
-	intpart = num / den;
-	if (intpart >= 0)num %= den;
-	else num = -num%den;
-	return *this;
-}
-Fraction inverted()
-{
-	Fraction inverted = *this;
-	inverted.to_improper();
-	swap(inverted.num, inverted.den);
-	return inverted;
-}
-void print()const
-{
-	if (intpart)cout << intpart;
-	if (num)
-	{
-		if (intpart)cout << "(";
-		cout << num << "/" << den;
-		if (intpart)cout << ")";
+		return num;
 	}
-	else if (intpart == 0) cout << 0;
-	cout << endl;
-}
-         //OPERTORS
-Fraction& operator=(const Fraction& other)
-{
-	intpart = other.intpart;
-	num = other.num;
-	den = other.den;
-	cout << "CopyAssignment: \t" << this << endl;
-	return *this;
-}
-Fraction& operator++() 
-{
-	intpart++;
-	return *this;
-}
-Fraction operator++(int)
-{
-	Fraction old = *this;
-	intpart++;
-	return old;
-}
-//{
-//	in >> intpart >> num >> den;
-//	return cin;
-//}
-Fraction& operator--()
-{
-	if (intpart != 0)intpart--;
-	else num -= den;
-	return *this;
-}
-Fraction& operator--(int)
-{
-	Fraction old = *this;
-	if (intpart != 0)intpart--;
-	else num -= den;
-	return old;
-}
-Fraction& operator+=(const Fraction& other)
-{
-	return *this = *this + other;
-}
-Fraction& operator-=(Fraction other)
-{
-	this->to_improper(); other.to_improper();
-	this->num = num * other.den - other.num * den;
-	this->den *= other.den;
-	return *this;
-}
-Fraction& operator*=(const Fraction& other)
-{
-	return *this = *this * other;
-}
-Fraction& operator/=(const Fraction& other)
-{
-	return *this = *this / other;
-}
+	int getDEN()const
+	{
+		return den;
+	}
+	int getINT()const
+	{
+		return intpart;
+	}
+	void setNUM(int num)
+	{
+		this->num = num;
+	}
+	void setDEN(int den)
+	{
+		if (den == 0)this->den = 1;
+		else this->den = den;
+	}
+	void setINT(int intpart)
+	{
+		this->intpart = intpart;
+	}
+	// CONSTRUCTOR
+	Fraction()
+	{
+		intpart = 0;
+		num = 0;
+		den = 1;
+		cout << "DefaultConstructor: \t" << this << endl;
+	}
+	Fraction(int numenator, int denominator)
+	{
+		intpart = 0;
+		num = numenator;
+		this->setDEN(denominator);
+		cout << "Constructor(NoInteger): \t" << this << endl;
+	}
+	explicit Fraction(int intpart)
+	{
+		this->intpart = intpart;
+		num = 0;
+		den = 1;;
+		cout << "Constructor(NoFraction): \t" << this << endl;
+	}
+	Fraction(double intpart)
+	{
+		this->intpart = intpart;
+		num=1, den = 1;
+
+	}
+	Fraction(int intpart, int numenator, int denominator)
+	{
+		this->intpart = intpart;
+		this->num = numenator;
+		setDEN(denominator);
+		cout << "Constructor: \t" << this << endl;
+	}
+	Fraction(const Fraction& other)
+	{
+		this->intpart = other.intpart;
+		this->num = other.num;
+		this->den = other.den;
+		cout << "CopyConstructor: \t" << this << endl;
+	}
+	//DESTRUCTOR
+	~Fraction()
+	{
+		cout << "Destructor: " << this << endl;
+	}
+	// TYPE CAST OPERATORS
+	explicit operator int()
+	{
+		return intpart;
+	}
+	operator double()
+	{
+		return intpart + (double)num / den;
+	}
+	// METHODS
+	Fraction& reduce()
+	{
+		int more, less, rest;
+		if (num < den)more = num, less = den;
+		else more = den, less = num;
+
+		do
+		{
+			rest = more % less;
+			more = less;
+			less = rest;
+		} while (rest);
+		int GSD = more; // gsd - greatest common divisor
+		num /= GSD;
+		den /= GSD;
+		return*this;
+	}
+	Fraction& to_improper()
+	{
+		num = intpart * den + (intpart >= 0 ? num : -num);
+		intpart = 0;
+		return *this;
+	}
+	Fraction& to_proper()
+	{
+		intpart = num / den;
+		if (intpart >= 0)num %= den;
+		else num = -num % den;
+		return *this;
+	}
+	Fraction inverted()
+	{
+		Fraction inverted = *this;
+		inverted.to_improper();
+		swap(inverted.num, inverted.den);
+		return inverted;
+	}
+	void print()const
+	{
+		if (intpart)cout << intpart;
+		if (num)
+		{
+			if (intpart)cout << "(";
+			cout << num << "/" << den;
+			if (intpart)cout << ")";
+		}
+		else if (intpart == 0) cout << 0;
+		cout << endl;
+	}
+	//OPERTORS
+	Fraction& operator=(const Fraction& other)
+	{
+		intpart = other.intpart;
+		num = other.num;
+		den = other.den;
+		cout << "CopyAssignment: \t" << this << endl;
+		return *this;
+	}
+	Fraction& operator++()
+	{
+		intpart++;
+		return *this;
+	}
+	Fraction operator++(int)
+	{
+		Fraction old = *this;
+		intpart++;
+		return old;
+	}
+
+	Fraction& operator--()
+	{
+		if (intpart != 0)intpart--;
+		else num -= den;
+		return *this;
+	}
+	Fraction& operator--(int)
+	{
+		Fraction old = *this;
+		if (intpart != 0)intpart--;
+		else num -= den;
+		return old;
+	}
+	Fraction& operator+=(const Fraction& other)
+	{
+		return *this = *this + other;
+	}
+	Fraction& operator-=(Fraction other)
+	{
+		this->to_improper(); other.to_improper();
+		this->num = num * other.den - other.num * den;
+		this->den *= other.den;
+		return *this;
+	}
+	Fraction& operator*=(const Fraction& other)
+	{
+		return *this = *this * other;
+	}
+	Fraction& operator/=(const Fraction& other)
+	{
+		return *this = *this / other;
+	}
 };
 bool operator==(const Fraction& A, const Fraction& B);
 bool operator!=(const Fraction& A, const Fraction& B);
@@ -207,11 +210,15 @@ bool operator<(const Fraction& A, const Fraction& B);
 bool operator>=(const Fraction& A, const Fraction& B);
 bool operator<=(const Fraction& A, const Fraction& B);
 std::ostream& operator << (std::ostream& cout, const Fraction& obj);
-std::istream& operator >> (std::istream& in ,Fraction& obj);
+std::istream& operator >> (std::istream& in, Fraction& obj);
 void main()
 {
 #define ConstructorCheck
 #define OperatorCheck
+#define StreamCheck
+#define conversion_from_other_to_class
+#define conversion_from_class_to_other
+#define conversionTask
 
 #ifdef ConstructorCheck1
 	setlocale(LC_ALL, "");
@@ -219,28 +226,48 @@ void main()
 	A.print();
 	Fraction B(5);
 	B.print();
-	Fraction C=(1,2);
+	Fraction C = (1, 2);
 	C.print();
-	Fraction D(2,3,4);
+	Fraction D(2, 3, 4);
 	D.print();
 	Fraction F(2, 5);
 	F.selint();
 	F.print();
 #endif
 #ifdef OperatorCheck1
-	Fraction A(-1,2,3);
-	Fraction B(1,3);
+	Fraction A(-1, 2, 3);
+	Fraction B(1, 3);
 	A.print();
 	B.print();
 	A *= B;
-    A /= B;
+	A /= B;
 	A.reduce();
 	A.print();
 #endif
-
-	Fraction A(2,3,4);
+#ifdef StreamCheck1
+	Fraction A(2, 3, 4);
 	cout << "Enter your fraction: "; cin >> A;
 	cout << "Fraction: " << A << endl;
+#endif
+#ifdef conversion_from_other_to_class1
+	Fraction A = (Fraction)5;
+	cout << A;
+	cout << delimeter;
+	Fraction B;
+	B = Fraction(8);
+	cout << dbldelimeter;
+	cout << B;
+#endif 
+#ifdef conversionTask1
+	Fraction A(2,3,4);
+	cout << A << endl;
+	double a = A;
+	cout << a << endl;
+#endif 
+#ifdef conversionTask
+	Fraction B = 2.75345;
+	cout << B << endl;
+#endif
 }
 std::ostream& operator << (std::ostream& os, const Fraction& obj)
 {
@@ -253,7 +280,7 @@ std::ostream& operator << (std::ostream& os, const Fraction& obj)
 	}
 	return os;
 }
-std::istream& operator >> (std::istream& is,  Fraction& obj)
+std::istream& operator >> (std::istream& is, Fraction& obj)
 {
 	const int SIZE = 64;
 	char buffer[SIZE]{};
@@ -267,9 +294,9 @@ std::istream& operator >> (std::istream& is,  Fraction& obj)
 	for (int i = 0; i < n; i++)cout << number[i] << " "; cout << endl;
 	switch (n)
 	{
-	    case 1: obj = Fraction(number[0]); break;
-		case 2: obj = Fraction(number[0], number[1]); break;
-		case 3: obj = Fraction(number[0], number[1],number[2]); break;
+	case 1: obj = Fraction(number[0]); break;
+	case 2: obj = Fraction(number[0], number[1]); break;
+	case 3: obj = Fraction(number[0], number[1], number[2]); break;
 	}
 	return is;
 }
@@ -277,15 +304,15 @@ Fraction operator+(const Fraction& A, const Fraction& B)
 {
 	return Fraction
 	(
-		A.getINT() + B.getINT(), 
-		B.getDEN()* A.getNUM()+ A.getDEN() * B.getNUM(),
+		A.getINT() + B.getINT(),
+		B.getDEN() * A.getNUM() + A.getDEN() * B.getNUM(),
 		A.getDEN() * B.getDEN()
 	);
 }
-Fraction operator*(Fraction A,Fraction B)
+Fraction operator*(Fraction A, Fraction B)
 {
 	A.to_improper(); B.to_improper();
-	return Fraction(A.getNUM() * B.getNUM(),A.getDEN() * B.getDEN()).to_proper();
+	return Fraction(A.getNUM() * B.getNUM(), A.getDEN() * B.getDEN()).to_proper();
 }
 Fraction operator/(const Fraction& A, Fraction B)
 {
@@ -295,7 +322,7 @@ Fraction operator/(const Fraction& A, Fraction B)
 bool operator==(const Fraction& A, const Fraction& B)
 {
 	return A.getINT() == B.getINT() &&
-		B.getDEN()* A.getNUM() == A.getDEN() * B.getNUM();
+		B.getDEN() * A.getNUM() == A.getDEN() * B.getNUM();
 }
 bool operator!=(const Fraction& A, const Fraction& B)
 {
---
+++ String/main.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+using namespace std;
+#define delimeter "\n===================================\n"
+
+class String
+{
+	int size; //size of string in Bytes
+	char* str; //adress of string in dynamic memory
+public:
+String(int size = 80)
+{
+		this->size = size;
+		this->str = new char[size]{};
+		cout << "DefaultConstructor:\t" << this << endl;
+}
+String(const char arr[])
+{
+	int i = 0;
+	while(arr[i] != '\0')i++;
+	size = ++i;
+	this->str = new char[size];
+	for (int i = 0; i < size; i++)str[i] = arr[i];
+}
+~String()
+{
+	delete[] this->str;
+	cout << "Destructor:\t\t" << this << endl;
+}
+          //METHODS
+char* get_()const
+{
+	return str;
+}
+int get_s()const
+{
+	return size;
+}
+void print()const
+{
+	cout << "Size:\t" << size << endl;
+	cout << "Str:\t" << str << endl;
+}
+};
+String operator+(const String& str1, const String& str2);
+std::ostream& operator << (std::ostream& os, const String& obj);
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	String str1 = "Hello";
+	String str2 = "World";
+	cout << str1 << endl;
+	cout << str2 << endl;
+	String str3 = str1 + str2;
+}
+std::ostream& operator << (std::ostream& os, const String& obj)
+{
+	for (int i = 0; i < obj.get_s(); i++)os << *(obj.get_() + i);
+	return os;
+}
+String operator+(const String& str1, const String& str2)
+{
+	int newsize = str1.get_s() + str2.get_s();
+	String str = newsize;
+	for (int i = 0; i < str1.get_s(); i++)*(str.get_() + i) = *(str1.get_() + i);
+	for (int i = str1.get_s(); i < newsize; i++)*(str.get_() + i) = *(str2.get_() +( i- str1.get_s()));
+	return str;
+}
