c2301151c846d45f66e4b74900748c0739567afe
Nazarets <ksenianazarec995@mail.com>
2024-06-14 16:30:25 +0300

    added copy and assignment for String

--- String/main.cpp
+++ String/main.cpp
@@ -7,27 +7,7 @@ class String
 	int size; //size of string in Bytes
 	char* str; //address of string in dynamic memory
 public:
-	String(int size = 80)
-	{
-		this->size = size;
-		this->str = new char[size] {};
-		cout << "DefaultConstructor:\t" << this << endl;
-	}
-	String(const char arr[])
-	{
-		int i = 0;
-		while (arr[i] != '\0')i++;
-		size = i;
-
-		this->str = new char[size];
-		for (int i = 0; i < size; i++)str[i] = arr[i];
-	}
-	~String()
-	{
-		delete[] this->str;
-		cout << "Destructor:\t\t" << this << endl;
-	}
-	//METHODS
+	//INCAPSULATION
 	char* get_()const
 	{
 		return str;
@@ -36,10 +16,46 @@ public:
 	{
 		return size;
 	}
+	String(int size = 80)
+	{
+		this->size = size;
+		this->str = new char[size] {};
+		cout << "DefaultConstructor:\t" << this << endl;
+	}
+	String(const char arr[])
+	{
+		this->size = strlen(arr) + 1;
+		this->str = new char[size] {};
+		for (int i = 0; arr[i]; i++)str[i] = arr[i];
+		cout << "Constructor:\t\t" << this << endl;
+	}
+	String(const String& other)
+	{
+		this->size = other.size;
+		this->str = new char[size] {};
+		for (int i = 0; other.str[i]; i++)str[i] = other.str[i];
+		cout << "CopyConstructor:\t\t" << this << endl;
+	}
+	~String()
+	{
+		delete[] this->str;
+		cout << "Destructor:\t\t" << this << endl;
+	}
+	//METHODS
 	void print()const
 	{
 		cout << "Size:\t" << size << endl;
 		cout << "Str:\t" << str << endl;
+	}
+	// OPERATORS
+	String& operator=(const String& other)
+	{
+		delete[] this->str;
+		this->size = other.size;
+		this->str = new char[size] {};
+		for (int i = 0; other.str[i]; i++)str[i] = other.str[i];
+		cout << "CopyAssignment:\t\t" << this << endl;
+		return *this;
 	}
 };
 String operator+(const String& str1, const String& str2);
@@ -57,14 +73,13 @@ void main()
 }
 std::ostream& operator << (std::ostream& os, const String& obj)
 {
-	for (int i = 0; i < obj.get_s(); i++)os << *(obj.get_() + i);
-	return os;
+	return os << obj.get_();
 }
 String operator+(const String& str1, const String& str2)
 {
 	int newsize = str1.get_s() + str2.get_s();
-	String str = newsize;
-	for (int i = 0; i < str1.get_s(); i++)*(str.get_() + i) = *(str1.get_() + i);
-	for (int i = str1.get_s(); i < newsize; i++)*(str.get_() + i) = *(str2.get_() + (i - str1.get_s()));
+	String str = newsize-1;
+	for (int i = 0; i < str1.get_s(); i++) *(str.get_() + i) = *(str1.get_() + i);
+	for (int i = str1.get_s()-1, d=0 ; i < newsize; i++,d++) *(str.get_() + i) = *(str2.get_()+d); 
 	return str;
 }
