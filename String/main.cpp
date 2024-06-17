b305f85937d7f4ae955f9d1573f5f13d24da2159
Nazarets <ksenianazarec995@mail.com>
2024-06-17 22:45:10 +0300

    methods're outta class

--- String/main.cpp
+++ String/main.cpp
@@ -8,65 +8,80 @@ class String
 	char* str; //address of string in dynamic memory
 public:
 	//INCAPSULATION
-	const char* get_()const
-	{
-		return str;
-	}
-	char* get_()
-	{
-		return str;
-	}
-	int get_s()const
-	{
-		return size;
-	}
-	explicit String(int size = 80) : size(size), str(new char[size]{})
-	{
-		//this->size = size; defined upper
-		//this->str = new char[size] {};
-		cout << "DefaultConstructor:\t" << this << endl;
-	}
-	String(const char arr[]) : String(strlen(arr)+1)
-	{
-		//this->size = strlen(arr) + 1;
-		//this->str = new char[size] {};
-		for (int i = 0; arr[i]; i++)str[i] = arr[i];
-		cout << "Constructor:\t\t" << this << endl;
-	}
+	const char* get_()const;
+	char* get_();
+	int get_s()const;
+	explicit String(int size = 80);
+	String(const char arr[]);
 	//COPY CONSTRUCTOR
-	String(const String& other): String(other.str) // we have this so we don't need code lower(except cout, oc)
-	{
-		//this->size = other.size;
-		//this->str = new char[size] {};
-		//for (int i = 0; i<size; i++)str[i] = other.str[i];
-		cout << "CopyConstructor:\t\t" << this << endl;
-	}
-	~String()
-	{
-		delete[] this->str;
-		cout << "Destructor:\t\t" << this << endl;
-	}
+	String(const String& other);
+	~String();
 	//METHODS
-	void print()const
-	{
-		cout << "Obj:\t" << size << "\t";
-		cout << "Size:\t" << size <<"\t";
-		cout << "Str:\t" << str << "\t";
-		cout << "Addr:\t" << &str <<"\t";
-		cout << "\n\n";
-	}
+	void print()const;
 	// OPERATORS
-	String& operator=(const String& other)
-	{
-		if (this == &other) return *this;
-		delete[] str;
-		this->size = other.size;
-		this->str = new char[size] {};
-		for (int i = 0; other.str[i]; i++)str[i] = other.str[i];
-		cout << "CopyAssignment:\t\t" << this << endl;
-		return *this;
-	}
+	String& operator=(const String& other);
 };
+//INCAPSULATION
+const char* String::get_()const
+{
+	return str;
+}
+char* String::get_()
+{
+	return str;
+}
+int String::get_s()const
+{
+	return size;
+}
+String::String(int size) : size(size), str(new char[size] {})
+{
+	//this->size = size; defined upper
+	//this->str = new char[size] {};
+	cout << "DefaultConstructor:\t" << this << endl;
+}
+String::String(const char arr[]) : String(strlen(arr) + 1)
+{
+	//this->size = strlen(arr) + 1;
+	//this->str = new char[size] {};
+	for (int i = 0; arr[i]; i++)str[i] = arr[i];
+	cout << "Constructor:\t\t" << this << endl;
+}
+//COPY CONSTRUCTOR
+String::String(const String& other) : String(other.str) // we have this so we don't need code lower(except cout, oc)
+{
+	//this->size = other.size;
+	//this->str = new char[size] {};
+	//for (int i = 0; i<size; i++)str[i] = other.str[i];
+	cout << "CopyConstructor:\t\t" << this << endl;
+}
+String::~String()
+{
+	delete[] this->str;
+	cout << "Destructor:\t\t" << this << endl;
+}
+//METHODS
+void String:: print()const
+{
+	cout << "Obj:\t" << this << "\t";
+	cout << "Size:\t" << size << "\t";
+	cout << "Str:\t" << str << "\t";
+	cout << "Addr:\t" << &str << "\t";
+	cout << "\n\n";
+}
+// OPERATORS
+String& String::operator=(const String& other)
+{
+	if (this == &other) return *this;
+	delete[] str;
+	this->size = other.size;
+	this->str = new char[size] {};
+	for (int i = 0; other.str[i]; i++)str[i] = other.str[i];
+	cout << "CopyAssignment:\t\t" << this << endl;
+	return *this;
+}
+
+
 String operator+(const String& str1, const String& str2);
 std::ostream& operator << (std::ostream& os, const String& obj);
 
